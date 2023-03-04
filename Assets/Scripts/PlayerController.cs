84c8bc169c1e111d80f7d8ed008b050ffdd40c20
jungtaek kim <kgajungtaek@gmail.com>
2023-03-05 01:30:24 +0900

    feat : 플레이어 점프 구현
    플레이어의 점프 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField]
+	private float moveSpeed;
+	[SerializeField]
+	private float jumpSpeed;
+
+	private new Rigidbody2D rigidbody;
+	private Animator animator;
+
+	private void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		Rotate();
+	}
+
+	private void OnJump(InputValue value)
+	{
+		Jump();
+	}	
+
+	private void Jump()
+	{
+		// rigidbody.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+		rigidbody.velocity = Vector2.up * jumpSpeed;
+	}
+
+	private void Rotate()
+	{
+		transform.right = rigidbody.velocity + Vector2.right * moveSpeed;
+	}
+}
